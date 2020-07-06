119080a0435443f58f38982da6c150c18f336a78
Eriyeza <eriyeza@google.com>
2020-07-06 16:12:59 +0000

    Add documentation for the UserDao interface.

--- src/main/java/com/google/capstone/dao/UserDao.java
+++ src/main/java/com/google/capstone/dao/UserDao.java
@@ -1,9 +1,33 @@
+// TODO: Not entirely sure how interacting with user data will be but created a boiler plate.
 package com.google.capstone.dao;
 
 import com.google.capstone.data.User;
 
+/**
+ * An interface for retrieving and manipulating data about Users.
+ */
 public interface UserDao {
+
+  /** 
+   * Retrieves a User object from a data source.
+   *
+   * @param id The unique id that identifies the user.
+   * @return A User object containg data that represents the user and their preferences.
+   */
   public User getUser(String id);
+
+  /**
+   * Adds a User to the data source.
+   *
+   * @param user The user being added to the data source.
+   */
   public void addUser(User user);
+
+  /**
+   * Updates the user's settings and registers the change in the data source.
+   *
+   * @param user The user object that contains the new settings.
+   * @return Whether or not the transaction was successful.
+   */
   public boolean updateUserSettings(User user);
 }
