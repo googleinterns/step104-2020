2aa164968e13387f7b84e773844b8e0679324de4
Eriyeza <eriyeza@google.com>
2020-07-06 15:29:31 +0000

    Change Release objects to store sdk release names as opposed to entire objects.

--- src/main/java/com/google/capstone/data/Release.java
+++ src/main/java/com/google/capstone/data/Release.java
@@ -5,9 +5,9 @@ import java.util.HashMap;
 public class Release {
   private String platform;
   private String releaseNum;
-  private HashMap<String, SDKRelease> enrolledSDKs;
+  private HashMap<String, String> enrolledSDKs;
 
-  public Release(String platform, String releaseNum, HashMap<String, SDKRelease> enrolledSDKs) {
+  public Release(String platform, String releaseNum, HashMap<String, String> enrolledSDKs) {
     this.platform = platform;
     this.releaseNum = releaseNum;
     this.enrolledSDKs = enrolledSDKs;
