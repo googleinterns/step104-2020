a8a19f08840682b8b3bd49ea83d8e09803de5d8b
Eriyeza <eriyeza@google.com>
2020-07-08 19:55:17 +0000

    Fix indentation in Main.java and add json dependencies to pom.xml

--- src/main/java/com/google/capstone/Main.java
+++ src/main/java/com/google/capstone/Main.java
@@ -6,7 +6,7 @@ import org.glassfish.jersey.server.ResourceConfig;
 @ApplicationPath("v1")
 public class Main extends ResourceConfig {
 
-    public Main() {
-        packages("com.google.capstone.api");
-    }
+  public Main() {
+    packages("com.google.capstone.api");
+  }
 }
