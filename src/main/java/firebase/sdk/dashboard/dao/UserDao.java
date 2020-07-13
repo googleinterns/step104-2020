af114963451d8d19ac422ac3c30e499454358dab
Eriyeza <eriyeza@google.com>
2020-07-13 23:59:20 +0000

    Replace high level functions with updateUser.

--- src/main/java/firebase/sdk/dashboard/dao/UserDao.java
+++ src/main/java/firebase/sdk/dashboard/dao/UserDao.java
@@ -26,29 +26,9 @@ public interface UserDao {
   public void addUser(User user);
 
   /**
-   * Adds the given sdk to the users favorites. 
+   * Registers the changes in the User object to the database.
    *
-   * @param user The user making the request.
-   * @param sdk The sdk the user wants to add to their favorites.
-   * @return Whether or not the transaction was successful.
+   * @param user The user being update in the data source.
    */
-  public boolean addSDKToFavorites(User user, SDK sdk);
-
-  /**
-   * Removes the given sdk to the users favorites. 
-   *
-   * @param user The user making the request.
-   * @param sdk The sdk the user wants to remove from their favorites.
-   * @return Whether or not the transaction was successful.
-   */
-  public boolean removeSDKFromFavorites(User user, SDK sdk);
-
-  /**
-   * Get the SDK names that the user marked as their favorite.
-   *
-   * @param user The user requesting to see their favourites
-   * @return A map containing only strings with the keys being platforms
-   * and the values being the names of the sdks.
-   */
-  public Map<String, String> getUserFavorites(User user);
+  public void updateUser(User user);
 }
