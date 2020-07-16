a7e2f4a09bfa102caa0f32ce29ba4b15f029636d
Timothy Mazenge <timmarz@google.com>
2020-07-16 07:19:06 +0000

    Implement the getPlatforms method

--- src/main/java/firebase/sdk/dashboard/dao/PlatformReleaseDaoDatastore.java
+++ src/main/java/firebase/sdk/dashboard/dao/PlatformReleaseDaoDatastore.java
@@ -20,20 +20,47 @@ public class PlatformReleaseDaoDatastore implements PlatformReleaseDao {
     
     public static final platformDatastore = DatastoreServiceFactory.getDatastoreService();
 
-    public List<Platform> getPlatforms(){
+    private Map<String, Platform> enumMapping = new HashMap<String, Platform>();
+    enumMapping.put("iOS", IOS);
+    enumMapping.put("Web", WEB);
+    enumMapping.put("Games", GAMES);
+    enumMapping.put("Android", ANDROID);
 
-        List<Platform> platforms = new new ArrayList<>();
+    public List<Platform> getPlatforms(){
+        List<Platform> platforms = new ArrayList<>();
+        
+
         Query<Entity> query = Query.newEntityQueryBuilder().setKind("Platform").build();
         
         PreparedQuery results = datastore.prepare(query);
 
         for (Entity result : results.asIterable()) {
-            String  = result.getProperty("name");
+            String name = result.getProperty("name");
+            Platform platform = enumMapping.get(name);
+            platforms.add(platform);
         }
         return platforms;  
     }
 
     public List<Release> getPlatformReleases(Platform platform) {
+        List<Release> releases = new ArrayList<>();
+
+        Query<Entity> query = Query.newEntityQueryBuilder().setKind("Release").build();
+        PreparedQuery releaseQuery = datastore.prepare(query);
+
+        for (Entity releaseEntity : releaseQuery.asIterable()) {
+
+            /* setproperty filter*/
+            String name = resultEntity.getProperty("platform");
+
+            Platform platform = enumMapping.get(name);
+            String releaseName = resultEntity.getProperty("releaseName");
+            String releaseManager = resultEntity.getProperty("release-manger");
+            String buganizerHotlistLink = resultEntity.getProperty("buganizerHotlistLink");
+            String launchCalDeadline = resultEntity.getProperty("launchCalDeadline");
+            String codeFreezeTime = resultEntity.getProperty("codeFreezeTime");
+        }
+
 
     }
 
@@ -44,4 +71,26 @@ public class PlatformReleaseDaoDatastore implements PlatformReleaseDao {
     public void deleteRelease(Platform platform, String releaseName){
 
     }
-}
+
+
+
+  public abstract Platform platform();
+
+  public abstract String releaseName();
+
+  public abstract String releaseManager();
+
+  public abstract String buganizerHotlistLink();
+
+  /* launchCalDeadline is the date by which an Ariane launch entry should be approved
+   * for minor/major versions. */
+  public abstract Instant launchCalDeadline();
+
+  /* codeFreezeTime is when they cut off a branch to create a release candidate
+   * for enrolled SDKS which will later be tested by the SDK owners. Any code
+   * that is merged into the master branch after the code freeze date is not
+   * released ad part of that launch. */
+  public abstract Instant codeFreezeTime();
+
+
+
