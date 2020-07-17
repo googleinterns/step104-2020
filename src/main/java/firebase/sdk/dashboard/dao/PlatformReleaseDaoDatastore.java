e1c3726c0ccb7ac7b4fa17538adcdf435f8e6c23
Timothy Mazenge <timmarz@google.com>
2020-07-17 05:30:46 +0000

    Added constructor to the enum class

--- src/main/java/firebase/sdk/dashboard/dao/PlatformReleaseDaoDatastore.java
+++ src/main/java/firebase/sdk/dashboard/dao/PlatformReleaseDaoDatastore.java
@@ -4,7 +4,7 @@ package firebase.sdk.dashboard.dao;
 import firebase.sdk.dashboard.data.Release;
 import firebase.sdk.dashboard.data.SDKReleaseMetadata;
 import firebase.sdk.dashboard.data.Platform;
-import java.util.List;
+import java.util.*;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
@@ -18,7 +18,7 @@ import com.google.appengine.api.datastore.Query.SortDirection;
  */
 public class PlatformReleaseDaoDatastore implements PlatformReleaseDao {
     
-    public static final platformDatastore = DatastoreServiceFactory.getDatastoreService();
+    public static final dashboardDatastore = DatastoreServiceFactory.getDatastoreService();
 
     private Map<String, Platform> enumMapping = new HashMap<String, Platform>();
     enumMapping.put("iOS", IOS);
@@ -29,10 +29,9 @@ public class PlatformReleaseDaoDatastore implements PlatformReleaseDao {
     public List<Platform> getPlatforms(){
         List<Platform> platforms = new ArrayList<>();
         
-
         Query<Entity> query = Query.newEntityQueryBuilder().setKind("Platform").build();
         
-        PreparedQuery results = datastore.prepare(query);
+        PreparedQuery results = dashboardDatastore.prepare(query);
 
         for (Entity result : results.asIterable()) {
             String name = result.getProperty("name");
@@ -45,52 +44,70 @@ public class PlatformReleaseDaoDatastore implements PlatformReleaseDao {
     public List<Release> getPlatformReleases(Platform platform) {
         List<Release> releases = new ArrayList<>();
 
-        Query<Entity> query = Query.newEntityQueryBuilder().setKind("Release").build();
-        PreparedQuery releaseQuery = datastore.prepare(query);
+        Query<Entity> query = Query.newEntityQueryBuilder().setKind("Release").setFilter(PropertyFilter.eq("platform", platform))build();
+        PreparedQuery releaseQuery = dashboardDatastore.prepare(query);
 
         for (Entity releaseEntity : releaseQuery.asIterable()) {
 
             /* setproperty filter*/
             String name = resultEntity.getProperty("platform");
-
             Platform platform = enumMapping.get(name);
+
             String releaseName = resultEntity.getProperty("releaseName");
-            String releaseManager = resultEntity.getProperty("release-manger");
-            String buganizerHotlistLink = resultEntity.getProperty("buganizerHotlistLink");
+            String launchDate = resultEntity.getProperty("launchDate");
             String launchCalDeadline = resultEntity.getProperty("launchCalDeadline");
             String codeFreezeTime = resultEntity.getProperty("codeFreezeTime");
+            /*how to create a release o*/
+
+            Release release = Release.newBuilder()
+                .releaseName(releaseName)
+                .launchCalDeadline(launchCalDeadline)
+                .codeFreezeTime(codeFreezeTime)
+                .launchDate(launchDate).build();
+            releases.add(release);
         }
-
-
+        return release;
     }
 
     public void addRelease(Platform platform, Release release){
+        String platformString;
 
+         for (String key : enumMapping.keySet()) {
+            Platform value = enumMapping.get(key);
+            if (value == release.platform()) {
+                platformString = key;
+            }
+        }
+
+        Key releaseKey = dashboardDatastore.newKeyFactory()
+            .setKind("Release")
+            .newKey(release.releaseName() + platformString);
+        
+        Entity release = Entity.newBuilder(releaseKey)
+            .set("platform", platformString)
+            .set("releaseName", release.releaseName())
+            .set("releaseManager", release.releaseManager())
+            .set("launchDate", release.launchDate())
+            .set("buganizerHotlistLink", release.buganizerHotlistLink())
+            .set("launchCalDeadline", release.launchCalDeadline())
+            .set("codeFreezeTime", release.codeFreezeTime())
+            .build();
+
+        dashboardDatastore.put(release);
     }
 
     public void deleteRelease(Platform platform, String releaseName){
+        String platformString;
 
+        for (String key : enumMapping.keySet()) {
+            Platform value = enumMapping.get(key);
+            if (value == release.platform()) {
+                platformString = key;
+            }
+        }
+        
+        dashboardDatastore.delete(releaseName + platformString);
     }
-
-
-
-  public abstract Platform platform();
-
-  public abstract String releaseName();
-
-  public abstract String releaseManager();
-
-  public abstract String buganizerHotlistLink();
-
-  /* launchCalDeadline is the date by which an Ariane launch entry should be approved
-   * for minor/major versions. */
-  public abstract Instant launchCalDeadline();
-
-  /* codeFreezeTime is when they cut off a branch to create a release candidate
-   * for enrolled SDKS which will later be tested by the SDK owners. Any code
-   * that is merged into the master branch after the code freeze date is not
-   * released ad part of that launch. */
-  public abstract Instant codeFreezeTime();
 
 
 
--- src/main/java/firebase/sdk/dashboard/data/Platform.java
+++ src/main/java/firebase/sdk/dashboard/data/Platform.java
@@ -1,9 +1,20 @@
 package firebase.sdk.dashboard.data;
 
 public enum Platform {
-  ANDROID,
-  IOS,
-  WEB,
-  GAMES
+  ANDROID("Android"),
+  IOS("iOS"),
+  WEB("Web"),
+  GAMES("Games")
+
+  private String name;
+
+  Platform(Sting enumName){
+    this.name;
+  }
+
+  public String getName() {
+      return name;
+  }
+
 }
 
