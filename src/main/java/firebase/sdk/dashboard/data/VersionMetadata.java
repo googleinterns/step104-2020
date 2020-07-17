d65399f80344488d37e571d2bca68af8c29ada3a
Eriyeza <eriyeza@google.com>
2020-07-17 15:22:41 +0000

    Add javadoc specifying how VersionMetadata is different from SDKReleaseMetadata.

--- src/main/java/firebase/sdk/dashboard/data/VersionMetadata.java
+++ src/main/java/firebase/sdk/dashboard/data/VersionMetadata.java
@@ -3,6 +3,16 @@ package firebase.sdk.dashboard.data;
 import com.google.auto.value.AutoValue;
 import java.time.Instant;
 
+/**
+ * This differs from SDKReleaseMEtadata in the attributes it contains
+ * and when the API will return this data. This data will be displayed
+ * in the SDK version history page for a given sdk and represents a 
+ * row in the table. It is missing some data such as the additional info
+ * and what the previous version was from the SDKReleaseMetadata object
+ * and includes the launch date of the version. This will also be used to
+ * represent upcomning versions and the launch date will be the projected 
+ * launch date of the release.
+ */
 @AutoValue
 public abstract class VersionMetadata {
 
