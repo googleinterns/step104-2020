84a153efe8c5ee2925d3794692b82321ce75d687
Eriyeza <eriyeza@google.com>
2020-07-17 15:33:02 +0000

    Get rid of email field from user and add comment to clarify map keys and values.

--- src/main/java/firebase/sdk/dashboard/data/User.java
+++ src/main/java/firebase/sdk/dashboard/data/User.java
@@ -13,18 +13,16 @@ public abstract class User {
 
   public abstract String uid();
 
-  public abstract String email();
-
   /* Example: {"Android": ["firebase-common", "firebase-common-ktx"]} */
-  public abstract Map<String, List<String>> favoriteSDKs();
+  public abstract Map<Platform, List<String>> favoriteSDKs();
 
   @AutoValue.Builder
   public interface Builder {
     Builder uid(String uid);
 
-    Builder email(String email);
-
-    Builder favoriteSDKs(Map<String, List<String>> favoriteSDKs);
+    /* The key is the platform and the value will be a list of the names of
+     * the users favorite SDKs for that platform. */ 
+    Builder favoriteSDKs(Map<Platform, List<String>> favoriteSDKs);
 
     User build();
   }
