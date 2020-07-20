a71b9d0db73a4eed0fa17d9e294a4e4e430c9a41
Eriyeza <eriyeza@google.com>
2020-07-20 22:22:55 +0000

    Add method templates for adding and deleting versionmetadata and retrieveing all sdks for given platform to sdkdao interface.

--- src/main/java/firebase/sdk/dashboard/dao/SDKDao.java
+++ src/main/java/firebase/sdk/dashboard/dao/SDKDao.java
@@ -2,6 +2,7 @@ package firebase.sdk.dashboard.dao;
 
 import firebase.sdk.dashboard.data.SDK;
 import firebase.sdk.dashboard.data.SDKReleaseMetadata;
+import firebase.sdk.dashboard.data.VersionMetadata;
 import firebase.sdk.dashboard.data.Release;
 import firebase.sdk.dashboard.data.Platform;
 import java.util.List;
@@ -20,6 +21,15 @@ public interface SDKDao {
    * data source.
    */
   public SDK getSDK(Platform platform, String libraryName);
+
+  /**
+   * Retrieves the names of all SDK objects in the given platform from a datasource.
+   *
+   * @param platform The sdks' operating platform (ie Android, iOS, etc,).
+   * @return An SDK object containg all the information about the SDK from the 
+   * data source.
+   */
+  public List<String> getSDKs(Platform platform);
 
   /**
    * Retrieves the names of all the sdks enrolled in the given release.
@@ -62,8 +72,7 @@ public interface SDKDao {
 
   //TODO: Define exception
   /**
-   * Creates a VersionMetadata object and adds it and the SDKReleaseMetada
-   * object to the data source.
+   * Adds the SDKReleaseMetadata object to the datasource.
    *
    * @param sdk The SDKReleaseMetadata object that is being added.
    */
@@ -71,13 +80,29 @@ public interface SDKDao {
 
   //TODO: Define exception
   /**
-   * Deletes the SDKReleaseMetadata and its corresponding VerionMetadata 
-   * from the datasource.
+   * Deletes the SDKReleaseMetadata object from the datasource.
    *
    * @param sdk The SDKReleaseMetadata object that is being deleted from the 
    * data source.
    */
   public void deleteSDKRelease(SDKReleaseMetadata sdk);
+
+  //TODO: Define exception
+  /**
+   * Adds the VersionMetadata object to the datasource.
+   *
+   * @param sdk The VersionMetadata object that is being added.
+   */
+  public void addSDKVersion(VersionMetadata sdkVersion);
+
+  //TODO: Define exception
+  /**
+   * Deletes the VersionMetadata object from the datasource.
+   *
+   * @param sdk The VersionMetadata object that is being deleted from the 
+   * data source.
+   */
+  public void deleteSDKVersion(VersionMetadata sdkVersion);
 
   /**
    * Updates the given SDKRelease object and registers the changes in the data 
