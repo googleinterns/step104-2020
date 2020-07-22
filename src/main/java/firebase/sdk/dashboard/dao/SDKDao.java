02d0381aa324c1b0818afd953d9a517ff52acb57
Eriyeza <eriyeza@google.com>
2020-07-22 22:03:46 +0000

    Retrieve up to date dao implementations.

--- src/main/java/firebase/sdk/dashboard/dao/SDKDao.java
+++ src/main/java/firebase/sdk/dashboard/dao/SDKDao.java
@@ -1,7 +1,11 @@
 package firebase.sdk.dashboard.dao;
 
 import firebase.sdk.dashboard.data.SDK;
-import firebase.sdk.dashboard.data.SDKRelease;
+import firebase.sdk.dashboard.data.SDKReleaseMetadata;
+import firebase.sdk.dashboard.data.VersionMetadata;
+import firebase.sdk.dashboard.data.Release;
+import firebase.sdk.dashboard.data.Platform;
+import java.util.List;
 
 /**
  * An interface for retrieving and manipulating data about SDKs.
@@ -16,7 +20,25 @@ public interface SDKDao {
    * @return An SDK object containg all the information about the SDK from the 
    * data source.
    */
-  public SDK getSDK(String platform, String libraryName);
+  public SDK getSDK(Platform platform, String libraryName);
+
+  /**
+   * Retrieves the names of all SDK objects in the given platform from a datasource.
+   *
+   * @param platform The sdks' operating platform (ie Android, iOS, etc,).
+   * @return A list of the names of all the sdks for the given platform.
+   */
+  public List<String> getSDKs(Platform platform);
+
+  /**
+   * Retrieves the names of all the sdks enrolled in the given release.
+   *
+   * @param platform The SDKRelease object's operating platform (ie Android, iOS, etc,).
+   * @param releaseName The name of the release the SDKRelease object is enrolled 
+   * in (ie M74, M75_hotfix, etc,).
+   * @return A list of the names of all the sdks enrolled in the requested release.
+   */
+  public List<String> getSDKsEnrolledInRelease(Platform platform, String releaseName);
 
   /**
    * Retrieves the SDKRelease object from a datasource.
@@ -25,17 +47,61 @@ public interface SDKDao {
    * @param releaseName The name of the release the SDKRelease object is enrolled 
    * in (ie M74, M75_hotfix, etc,).
    * @param libraryName The sdk's name.
-   * @return An SDKRelease object containing all the information about the SDKRelease from
+   * @return An SDKReleaseMetadata object containing all the information about the SDKRelease from
    * the data source.
    */
-  public SDKRelease getSDKRelease(String platform, String releaseName, String libraryName);
+  public SDKReleaseMetadata getSDKReleaseMetadata(Platform platform, String releaseName, String libraryName);
+
+  /**
+   * Adds the SDK to the data source.
+   *
+   * @param sdk The SDK being added.
+   */
+  public void addSDK(SDK sdk);
 
   /**
    * Removes the SDK from the data source.
    *
    * @param sdk The SDK being removed.
-   * @return Whether or not the SDK object was successfully removed from the
+   */
+  public void deleteSDK(SDK sdk);
+
+  /**
+   * Adds the SDKReleaseMetadata object to the datasource.
+   *
+   * @param sdkRelease The SDKReleaseMetadata object that is being added.
+   */
+  public void addSDKRelease(SDKReleaseMetadata sdkRelease);
+
+  /**
+   * Deletes the SDKReleaseMetadata object from the datasource.
+   *
+   * @param sdkRelease The SDKReleaseMetadata object that is being deleted from the 
    * data source.
    */
-  public boolean deleteSDK(SDK sdk);
+  public void deleteSDKRelease(SDKReleaseMetadata sdkRelease);
+
+  /**
+   * Adds the VersionMetadata object to the datasource.
+   *
+   * @param sdkVersion The VersionMetadata object that is being added.
+   */
+  public void addSDKVersion(VersionMetadata sdkVersion);
+
+  /**
+   * Deletes the VersionMetadata object from the datasource.
+   *
+   * @param sdkVersion The VersionMetadata object that is being deleted from the 
+   * data source.
+   */
+  public void deleteSDKVersion(VersionMetadata sdkVersion);
+
+  /**
+   * Updates the given SDKRelease object and registers the changes in the data 
+   * source.
+   *
+   * @param sdkRelease The SDKRelease after the changes were made.
+   */
+  /* TODO: P2 functionality
+  public void updateSDKEnrolledInRelease(SDKReleaseMetadata sdkRelease);*/
 }
