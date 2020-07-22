86cec93ea1dc864c748551e416951d97a2dfa208
Eriyeza <eriyeza@google.com>
2020-07-22 16:31:12 +0000

    Update javadocs in SDKDao interface.

--- src/main/java/firebase/sdk/dashboard/dao/SDKDao.java
+++ src/main/java/firebase/sdk/dashboard/dao/SDKDao.java
@@ -26,8 +26,7 @@ public interface SDKDao {
    * Retrieves the names of all SDK objects in the given platform from a datasource.
    *
    * @param platform The sdks' operating platform (ie Android, iOS, etc,).
-   * @return An SDK object containg all the information about the SDK from the 
-   * data source.
+   * @return A list of the names of all the sdks for the given platform.
    */
   public List<String> getSDKs(Platform platform);
 
@@ -48,20 +47,18 @@ public interface SDKDao {
    * @param releaseName The name of the release the SDKRelease object is enrolled 
    * in (ie M74, M75_hotfix, etc,).
    * @param libraryName The sdk's name.
-   * @return An SDKRelease object containing all the information about the SDKRelease from
+   * @return An SDKReleaseMetadata object containing all the information about the SDKRelease from
    * the data source.
    */
   public SDKReleaseMetadata getSDKReleaseMetadata(Platform platform, String releaseName, String libraryName);
 
-  //TODO: Define exception
   /**
-   * Adds the SDK from the data source.
+   * Adds the SDK to the data source.
    *
    * @param sdk The SDK being added.
    */
   public void addSDK(SDK sdk);
 
-  //TODO: Define exception
   /**
    * Removes the SDK from the data source.
    *
@@ -69,36 +66,32 @@ public interface SDKDao {
    */
   public void deleteSDK(SDK sdk);
 
-  //TODO: Define exception
   /**
    * Adds the SDKReleaseMetadata object to the datasource.
    *
-   * @param sdk The SDKReleaseMetadata object that is being added.
+   * @param sdkRelease The SDKReleaseMetadata object that is being added.
    */
-  public void addSDKRelease(SDKReleaseMetadata sdk);
+  public void addSDKRelease(SDKReleaseMetadata sdkRelease);
 
-  //TODO: Define exception
   /**
    * Deletes the SDKReleaseMetadata object from the datasource.
    *
-   * @param sdk The SDKReleaseMetadata object that is being deleted from the 
+   * @param sdkRelease The SDKReleaseMetadata object that is being deleted from the 
    * data source.
    */
-  public void deleteSDKRelease(SDKReleaseMetadata sdk);
+  public void deleteSDKRelease(SDKReleaseMetadata sdkRelease);
 
-  //TODO: Define exception
   /**
    * Adds the VersionMetadata object to the datasource.
    *
-   * @param sdk The VersionMetadata object that is being added.
+   * @param sdkVersion The VersionMetadata object that is being added.
    */
   public void addSDKVersion(VersionMetadata sdkVersion);
 
-  //TODO: Define exception
   /**
    * Deletes the VersionMetadata object from the datasource.
    *
-   * @param sdk The VersionMetadata object that is being deleted from the 
+   * @param sdkVersion The VersionMetadata object that is being deleted from the 
    * data source.
    */
   public void deleteSDKVersion(VersionMetadata sdkVersion);
@@ -107,10 +100,8 @@ public interface SDKDao {
    * Updates the given SDKRelease object and registers the changes in the data 
    * source.
    *
-   * @param oldSDKRelease The SDKRelease before the changes were made.
-   * @param newSDKRelease The SDKRelease after the changes were made.
-   * @return Whether or not the SDKRelease was successfully modified.
+   * @param sdkRelease The SDKRelease after the changes were made.
    */
   /* TODO: P2 functionality
-  public void updateSDKEnrolledInRelease(SDKRelease oldSDKRelease, SDKRelease newSDKRelease);*/
+  public void updateSDKEnrolledInRelease(SDKReleaseMetadata sdkRelease);*/
 }
