3c21e60c9105ca61c9897d111da1f77340c8bfd9
Eriyeza <eriyeza@google.com>
2020-07-22 19:19:51 +0000

    Get data objects from correct branch.

---
+++ src/main/java/firebase/sdk/dashboard/data/Platform.java
@@ -0,0 +1,25 @@
+package firebase.sdk.dashboard.data;
+
+public enum Platform {
+  UNKNOWN(0),
+  ANDROID(1),
+  IOS(2),
+  WEB(3),
+  GAMES(4);
+
+  private int value;
+  private static final Platform[] platformValues = Platform.values();
+
+  private Platform(int value) {
+    this.value = value;
+  }
+
+  public int getValue() {
+    return value;
+  }
+
+  public static Platform fromInteger(int x) {
+    return platformValues[x];
+  }
+}
+
