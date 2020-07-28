23c9d1551b67f5d918171aa9a890a41a18294dc4
Eriyeza <eriyeza@google.com>
2020-07-28 15:12:46 +0000

    Add jackson annotations to User pojo.

--- src/main/java/firebase/sdk/dashboard/data/User.java
+++ src/main/java/firebase/sdk/dashboard/data/User.java
@@ -1,27 +1,34 @@
 package firebase.sdk.dashboard.data;
 
-import com.google.auto.value.AutoValue;
 import java.util.List;
 import java.util.Map;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.google.auto.value.AutoValue;
 
 @AutoValue
+@JsonDeserialize(builder = AutoValue_User.Builder.class)
 public abstract class User {
 
   public static Builder newBuilder() {
     return new AutoValue_User.Builder();
   }
 
+  @Jsonproperty("uid")
   public abstract String uid();
 
   /* Example: {"Android": ["firebase-common", "firebase-common-ktx"]} */
+  @Jsonproperty("favoriteSDKs")
   public abstract Map<Platform, List<String>> favoriteSDKs();
 
   @AutoValue.Builder
   public interface Builder {
+    @Jsonproperty("uid")
     Builder uid(String uid);
 
     /* The key is the platform and the value will be a list of the names of
      * the users favorite SDKs for that platform. */ 
+    @Jsonproperty("favoriteSDKs")
     Builder favoriteSDKs(Map<Platform, List<String>> favoriteSDKs);
 
     User build();
