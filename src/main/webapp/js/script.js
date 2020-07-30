614e384076f54a2f639236006926d6e6fd7af5f5
Timothy Mazenge <timmazenge@gmail.com>
2020-07-30 15:00:11 -0500

    Add comments.

--- src/main/webapp/js/script.js
+++ src/main/webapp/js/script.js
@@ -84,113 +84,109 @@ function showOrHideNotes(version) {
 let doms = ["web", "android", "games", "ios"]
 
 async function getPlatforms() {
-    const response = await fetch('/v1/platforms');
-    const platforms = await response.json();
-  
-    count = 0;
-    for (let plat in platforms){
-       if (plat == doms[count]){
-        const divElement = document.getElementById(plat); 
-        const textNode = document.createTextNode(platforms[plat]); 
-        divElement.appendChild(textNode);
-       }
-       count+=1;
+  const response = await fetch('/v1/platforms');
+  const platforms = await response.json();
+
+  count = 0;
+  for (let plat in platforms){
+    if (plat == doms[count]){
+      const divElement = document.getElementById(plat); 
+      const textNode = document.createTextNode(platforms[plat]); 
+      divElement.appendChild(textNode);
+     }
+     count+=1;
     }
 }
 
 //Fetch release pages
 async function getReleases(platform) {
-   const response = await fetch(`/v1/platforms/${platform}/releases`);
-   const releases = await response.json();
+  const response = await fetch(`/v1/platforms/${platform}/releases`);
+  const releases = await response.json();
    
-    //Create text elements for each release detail
-    for (i = 0; i < releases.length; i++) {
-        let element = "#release-" + i;
-        let release = releases[i];
+  //Create text elements for each release detail
+  for (i = 0; i < releases.length; i++) {
+    let element = "#release-" + i;
+    let release = releases[i];
 
-        const divElement = document.querySelector(element);
-        
-        const name = release["releaseName"];
-        const textNode0 = document.createTextNode(name); 
-        divElement.appendChild(textNode0);
-       
-        divElement.appendChild(document.createElement("HR"));
+    const divElement = document.querySelector(element);
 
-        const deadline = release["launchDeadline"];
-        const textNode1 = document.createTextNode("Launch Deadline: " + getDate(1595424871)); 
-        divElement.appendChild(textNode1);
+    const name = release["releaseName"];
+    const textNode0 = document.createTextNode(name); 
+    divElement.appendChild(textNode0);
+    divElement.appendChild(document.createElement("HR"));
 
-        divElement.appendChild(document.createElement("HR"));
+    const deadline = release["launchDeadline"];
+    const textNode1 = document.createTextNode("Launch Deadline: " + getDate(1595424871)); 
+    divElement.appendChild(textNode1);
+    divElement.appendChild(document.createElement("HR"));
 
-        const coldfreeze = release["codeFreezeDate"];
-        const textNode3 = document.createTextNode("Cold Freeze Date: " + getDate(1595424871)); 
-        divElement.appendChild(textNode3);
+    const coldfreeze = release["codeFreezeDate"];
+    const textNode3 = document.createTextNode("Cold Freeze Date: " + getDate(1595424871)); 
+    divElement.appendChild(textNode3);
+    divElement.appendChild(document.createElement("HR"));
 
-        divElement.appendChild(document.createElement("HR"));
-
-        const launch = release["launchDate"];
-        const textNode4 = document.createTextNode("Launch Date: " + getDate(Date.now())); 
-        divElement.appendChild(textNode4); 
+    const launch = release["launchDate"];
+    const textNode4 = document.createTextNode("Launch Date: " + getDate(Date.now())); 
+    divElement.appendChild(textNode4); 
     }
 }
 
 // Fetch release pages
 async function getSDKs(platform, releaseName) {
-   const response = await fetch(`v1/platforms/${platform}/releases/${releaseName}/sdks`);
-   const sdks = await response.json();
-   
-   const listSDKs = sdks[releaseName];
+  const response = await fetch(`v1/platforms/${platform}/releases/${releaseName}/sdks`);
+  const sdks = await response.json();
+
+  const listSDKs = sdks[releaseName];
 
   // Create header for release
-   const headerElement = document.querySelector("#header");
-   const headerText = document.createTextNode(releaseName);
-   headerElement.appendChild(headerText);
+  const headerElement = document.querySelector("#header");
+  const headerText = document.createTextNode(releaseName);
+  headerElement.appendChild(headerText);
 
-   // Creating elements for all sdks
-   console.log(listSDKs);
-   for (i = 0; i < listSDKs.length; i++) {
-        let element = "sdk" + i;
-        console.log(element);
-        console.log(listSDKs[i]);
-        const divElement = document.getElementById(element);
-        const textNode = document.createTextNode(listSDKs[i]); 
-        divElement.appendChild(textNode);
-   }
-
+  // Creating elements for all sdks
+  console.log(listSDKs);
+  for (i = 0; i < listSDKs.length; i++) {
+      let element = "sdk" + i;
+      console.log(element);
+      console.log(listSDKs[i]);
+      const divElement = document.getElementById(element);
+      const textNode = document.createTextNode(listSDKs[i]); 
+      divElement.appendChild(textNode);
+  }
 }
 
 // Fetch Version History
 async function getVersionHistory(platform, sdkName) {
-    const response = await fetch(`v1/platforms/${platform}/sdks/${sdkName}`);
-    const versionHistory = await response.json();
-    console.log(versionHistory);
+  const response = await fetch(`v1/platforms/${platform}/sdks/${sdkName}`);
+  const versionHistory = await response.json();
+  console.log(versionHistory);
 
-    const name = versionHistory["libraryName"];
-    const owner = versionHistory["owner"];
-    const externalName = versionHistory["externalName"];
-    const libraryGroup = versionHistory["libraryGroup"];
-    // TODO: Fetch for the latest version from endpoint
-    const latestVersion = "19.2.9";
+  const name = versionHistory["libraryName"];
+  const owner = versionHistory["owner"];
+  const externalName = versionHistory["externalName"];
+  const libraryGroup = versionHistory["libraryGroup"];
+  // TODO: Fetch for the latest version from endpoint
+  const latestVersion = "19.2.9";
 
-    createNode(name, "#sdk-name");
-    createNode("External Name: " + externalName, "#external");
-    createNode("Latest Version: " + latestVersion, "#latest-version");
-    createNode("Library Group: " + libraryGroup, "#library-group");
-    createNode(owner, "#owner");
+  createNode(name, "#sdk-name");
+  createNode("External Name: " + externalName, "#external");
+  createNode("Latest Version: " + latestVersion, "#latest-version");
+  createNode("Library Group: " + libraryGroup, "#library-group");
+  createNode(owner, "#owner");
 }
 
 // Fetch favorite SDKS
 async function getFavoriteSdks(userID) {
-    const response = await fetch(`v1/users/${userID}/sdks`);
-    const favorites = await response.json();
-    // Build the links for favorite sdks
-    const favoriteSDK = document.getElementById('favorites');  
-        for (i = 0; i < favorites.length; i++) {
-            favoriteSDK.appendChild(
-                createListElement(favorites[i]));
-            favoriteSDK.appendChild(document.createElement("HR"));
-            console.log(favorites[i]);
-        }
+  const response = await fetch(`v1/users/${userID}/sdks`);
+  const favorites = await response.json();
+  // Build the links for favorite sdks
+  const favoriteSDK = document.getElementById('favorites');  
+  for (i = 0; i < favorites.length; i++) {
+      favoriteSDK.appendChild(
+          createListElement(favorites[i]));
+      favoriteSDK.appendChild(document.createElement("HR"));
+      console.log(favorites[i]);
+  }
 }
 
 // Create link for favorite sdk
@@ -203,14 +199,14 @@ function createListElement(text) {
 }
 
 function getDate(time) {
-    var date = new Date(time);
-    return date;
+  var date = new Date(time);
+  return date;
 }
 
 function createNode(tag, id) {
-    const tagElement = document.querySelector(id);
-    const tagText = document.createTextNode(tag);
-    tagElement.appendChild(tagText);
+  const tagElement = document.querySelector(id);
+  const tagText = document.createTextNode(tag);
+  tagElement.appendChild(tagText);
 }
 
 getPlatforms();
