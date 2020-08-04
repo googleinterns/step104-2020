92f8ddfc52939a49f743891227af959f8a3e4a02
Timothy Mazenge <timmazenge@gmail.com>
2020-08-04 09:43:04 -0500

    Fixed indentation

--- src/main/webapp/js/script.js
+++ src/main/webapp/js/script.js
@@ -135,12 +135,12 @@ async function getFavoriteSdks(userID) {
   const response = await fetch(`v1/users/${userID}/sdks`);
   const favorites = await response.json();
   // Build the links for favorite sdks
-  const favoriteSDK = document.getElementById('favorites');  
+  const favoriteSDK = document.getElementById('favorites');
+  console.log(favoriteSDK);
   for (i = 0; i < favorites.length; i++) {
-      favoriteSDK.appendChild(
-          createListElement(favorites[i]));
-      favoriteSDK.appendChild(document.createElement("HR"));
-      console.log(favorites[i]);
+    favoriteSDK.appendChild(createListElement(favorites[i]));
+    favoriteSDK.appendChild(document.createElement("HR"));
+    console.log(favorites[i]);
   }
 }
 
