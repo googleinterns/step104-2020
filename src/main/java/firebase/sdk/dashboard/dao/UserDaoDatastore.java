7421b0b07301e24e68a2ece5c8d05686a816f6de
Nnenna Dara <65994946+nnenna352@users.noreply.github.com>
2020-08-04 17:13:43 -0500

    Removed excess spaces

--- src/main/java/firebase/sdk/dashboard/dao/UserDaoDatastore.java
+++ src/main/java/firebase/sdk/dashboard/dao/UserDaoDatastore.java
@@ -26,11 +26,9 @@ import com.google.appengine.api.datastore.Query.CompositeFilterOperator;
 import com.google.appengine.api.datastore.FetchOptions;
 import com.google.appengine.api.datastore.QueryResultIterable;
 
-/* Class to implement UserDao to manipulate data about the users */
+/* Class to implement UserDao to manipulate data about the users */.
 public class UserDaoDatastore implements UserDao {
-
     public static final DatastoreService DATASTORE = DatastoreServiceFactory.getDatastoreService();
-
     // Get a user from the datastore
     public User getUser(String id){
         FilterPredicate userPropertyFilter = makePropertyFilter("id", id);
@@ -50,17 +48,14 @@ public class UserDaoDatastore implements UserDao {
         .build();
         return users;
     }
-
     // Add a user to the datastore
     public void addUser(User user){
        Key userKey = KeyFactory.createKey("User", user.uid());
-       
         // Create a user entity
         Entity userEntity = new Entity(userKey);
         userEntity.setProperty("id",user.uid());
         userEntity.setProperty("email",user.email());
         userEntity.setProptery("favoriteSDKs",user.favoriteSDKs());
-
         DATASTORE.put(userEntity);
     }
  
@@ -68,7 +63,6 @@ public class UserDaoDatastore implements UserDao {
     public void updateUser(User user){
          FilterPredicate userPropertyFilter = makePropertyFilter("id", user.uid());
         Query query = new Query("User").setFilter(userPropertyFilter);
-
         PreparedQuery preparedQuery = DATASTORE.prepare(query);
         FetchOptions fetchOptions = FetchOptions.Builder.withDefaults();
         Entity userEntity = preparedQuery.asSingleEntity();
@@ -77,10 +71,8 @@ public class UserDaoDatastore implements UserDao {
         }
          userEntity.setProperty("favoriteSDKs",user.favoriteSDKs());
          DATASTORE.put(userEntity);
-    }
-                                                                 
+    }                                                              
    private FilterPredicate makePropertyFilter(String property, Object value) {
         FilterPredicate propertyFilter = new FilterPredicate(property, FilterOperator.EQUAL, value);
-
         return propertyFilter;                                                              
 }
