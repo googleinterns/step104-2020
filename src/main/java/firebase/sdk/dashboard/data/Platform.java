4d4b8d401113d6d3bc06753cae8dd84bafcecb02
Eriyeza <eriyeza@google.com>
2020-08-05 17:01:39 +0000

    Fix issues with deplyment and change platforms to lowercase.

--- src/main/java/firebase/sdk/dashboard/data/Platform.java
+++ src/main/java/firebase/sdk/dashboard/data/Platform.java
@@ -5,10 +5,10 @@ import java.util.Map;
 
 public enum Platform {
   
-  ANDROID("Android"),
-  IOS("iOS"),
-  WEB("Web"),
-  GAMES("Games");
+  ANDROID("android"),
+  IOS("iaos"),
+  WEB("web"),
+  GAMES("games");
 
   private String label;
 
