8465e2f0c1e6b662775111dc737921b6ea56770c
Eriyeza <eriyeza@google.com>
2020-08-05 21:34:40 +0000

    Get rid of stretched links and unneeded variabel.

--- src/main/webapp/js/navigate.js
+++ src/main/webapp/js/navigate.js
@@ -1,5 +1,4 @@
 function goToPage(page, params) {
-  const params = new URLSearchParams(`platform=android&releaseName=M76&sdkName=${sdkName}`);
   const url = new URL(page, window.location.origin);
   window.location.href = `${url.toString()}?${params.toString()}`
 }
